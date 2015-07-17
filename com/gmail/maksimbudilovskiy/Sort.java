f6583baf300bc52a8243de484dcaedb468b1d20b
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-17 15:24:24 +0300

    Update Sort.java

--- com/gmail/maksimbudilovskiy/Sort.java
+++ com/gmail/maksimbudilovskiy/Sort.java
@@ -1,14 +1,11 @@
 package com.gmail.maksimbudilovskiy;
 
-public abstract class Sort {
+abstract public class Sort {
 	protected long timeout;
 	
 	public long getTimeout() {
 		return timeout;
 	}
 		
-	public int[] sorting(int[] array) {
-		timeout = 0;
-		return array;
-	}
+	abstract public int[] sorting(int[] array);
 }
